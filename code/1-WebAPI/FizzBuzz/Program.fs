9a4e2db8feb1b1ac2b5db9f320e8e4f42b9507e5
Grigoriy Belenkiy <grishace@gmail.com>
2016-10-25 18:26:08 -0600

    Adding WebAPI project

---
+++ code/1-WebAPI/FizzBuzz/Program.fs
@@ -0,0 +1,25 @@
+// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open Microsoft.Extensions.Configuration
+open Microsoft.AspNetCore.Hosting
+
+[<EntryPoint>]
+let main argv =
+    
+    let config = ConfigurationBuilder()
+                    .AddCommandLine(argv)
+                    .AddEnvironmentVariables("ASPNETCORE_")
+                    .Build()
+
+    let host = WebHostBuilder()
+                    .UseConfiguration(config)
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .Build()
+    host.Run();
+    
+    0 // return an integer exit code
